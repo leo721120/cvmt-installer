81a110caf69886151f6ed55f24a16b08e2b4333e
leo721120 <57064752+leo721120@users.noreply.github.com>
2025-05-27 13:42:30 +0800

    chore(env): initial commit
    add installation script

---
+++ cvmt.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+{ # this ensures the entire script is downloaded #
+
+do_install() {
+    echo "Installing CVMT..."
+}
+
+do_install
+
+} # this ensures the entire script is downloaded #
